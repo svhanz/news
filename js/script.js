0aba32b35886dbf6de9ba938d9e7509853ac2614
Sergey Shlegel <serg-69@mail.ru>
2022-04-03 19:45:22 +0300

    feat: add default.css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+const API_KEY = '3b6fc1a03e4a4b969d8fa8ce1a49547e';
+
 const choicesElem = document.querySelector('.js-choice');
 console.log(choicesElem);
-const choices = new Choices(choicesElem);
+const choices = new Choices(choicesElem, {
+  searchEnabled: false,
+  itemSelectText: '',
+});
+
+const loadNews = () => {
+  const data = getdata();
+  renderCard(data);
+
+}
